ac93b7d1733a69ea8843d26df83028136ad5f1ea
Manjunath D <manju.desappa@gmail.com>
2019-11-04 09:17:53 +0530

    Make / to public

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const IndexRoute = {
   path: '/',
   method: 'GET',
   config: {
+    auth: false,
     description: "This is an entrypoint to beauty.",
     tags: ["api", "public"],
   },
@@ -16,16 +17,16 @@ const PrivateRoutes = [
   ...ProductRoutes
 ];
 
-const ModifiedPrivateRoutes = PrivateRoutes.map(route => Object.assign({}, route, { 
-    config: {
-        ...route.config, 
-        validate: {
-          ...route.config.validate,
-            headers: Joi.object({
-                'authorization': Joi.string().required()
-            }).unknown() 
-        }
+const ModifiedPrivateRoutes = PrivateRoutes.map(route => Object.assign({}, route, {
+  config: {
+    ...route.config,
+    validate: {
+      ...route.config.validate,
+      headers: Joi.object({
+        'authorization': Joi.string().required()
+      }).unknown()
     }
+  }
 }));
 
 export default [
