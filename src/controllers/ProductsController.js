7c6c762c46b496bab92ddff9824ea50831fb2f41
Manjunath D <manju.desappa@gmail.com>
2019-11-04 08:28:23 +0530

    Fix products controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,16 +1,18 @@
-import { products } from '~/persistence';
+import { products as productsDB } from '~/persistence';
 
 const ProductService = {
     addProducts: async (request, h) => {
         try {
-            await products.add({
-                ...request.payload,
+            const products = request.payload;
+            const productsWithImage = products.map(prod => ({
+                ...prod,
                 image_url: "https://source.unsplash.com/100x100/?product",
                 imageSrcList: [
                     "https://source.unsplash.com/1366x768/?product",
                     "https://source.unsplash.com/1800x1000/?product"
                 ],
-            });
+            }));
+            await productsDB.insert(productsWithImage);
             return h.response();
         } catch (err) {
             logger.error(err);
@@ -19,7 +21,7 @@ const ProductService = {
     },
     getAllProducts: async (request, h) => {
         try {
-            return h.response(await products.find().execAsync());
+            return h.response(await productsDB.find().execAsync());
         } catch (err) {
             logger.error(err);
             return h.response().code(500);
@@ -27,7 +29,7 @@ const ProductService = {
     },
     updateProduct: async ({ params, payload }, h) => {
         try {
-            const updated = await products.updateOne({ _id: params.product_id }, payload, { returnUpdatedDocs: true })
+            const updated = await productsDB.updateOne({ _id: params.product_id }, payload, { returnUpdatedDocs: true })
             return h.response(updated);
         } catch (err) {
             logger.error(err);
--- src/routes/products-route.js
+++ src/routes/products-route.js
@@ -5,42 +5,40 @@ import ProductsController from '~/controllers/ProductsController';
 
 
 export default [
-    {
-      path: '/products',
-      method: 'POST',
-      config: {
-        description: 'Add multiple products',
-        tags: ['api', 'products'],
-        validate: {
-          payload: Joi.array().min(1).max(100).required().items(Product).label("ProductList"),
-        },
+  {
+    path: '/products',
+    method: 'POST',
+    config: {
+      description: 'Add multiple products',
+      tags: ['api', 'products'],
+      validate: {
+        payload: Joi.array().min(1).max(100).required().items(Product).label("ProductList"),
       },
-      handler: ProductsController.addProducts,
     },
-    {
-      path: '/product/{product_id}',
-      method: 'PUT',
-      config: {
-          description: 'Edit product information',
-          tags: ['api', 'products'],   
-          validate: {
-              payload: Product,
-              params: {
-                product_id: Joi.string().max(8).required()
-              }
-          },
+    handler: ProductsController.addProducts,
+  },
+  {
+    path: '/product/{product_id}',
+    method: 'PUT',
+    config: {
+      description: 'Edit product information',
+      tags: ['api', 'products'],
+      validate: {
+        payload: Product,
+        params: {
+          product_id: Joi.string().max(8).required()
+        }
       },
-      handler: ProductsController.updateProduct,
     },
-    {
-      path: '/products',
-      method: 'GET',
-      config: {
-        description: 'Get all products in the inventory',
-        tags: ['api', 'products'],
-        auth: 'jwt'
-      },
-      handler: ProductsController.getAllProducts,
+    handler: ProductsController.updateProduct,
+  },
+  {
+    path: '/products',
+    method: 'GET',
+    config: {
+      description: 'Get all products in the inventory',
+      tags: ['api', 'products'],
     },
-  ];
-  
+    handler: ProductsController.getAllProducts,
+  },
+];
