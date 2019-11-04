b296e1b14d21b9117a0380fae244aa6fa046d5f1
Manjunath D <manju.desappa@gmail.com>
2019-11-04 08:02:36 +0530

    Add some image data while adding products

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -3,9 +3,16 @@ import { products } from '~/persistence';
 const ProductService = {
     addProducts: async (request, h) => {
         try {
-            await products.add(request.payload)
+            await products.add({
+                ...request.payload,
+                image_url: "https://source.unsplash.com/100x100/?product",
+                imageSrcList: [
+                    "https://source.unsplash.com/1366x768/?product",
+                    "https://source.unsplash.com/1800x1000/?product"
+                ],
+            });
             return h.response();
-        }catch(err) {
+        } catch (err) {
             logger.error(err);
             return h.response().code(500);
         }
@@ -13,7 +20,7 @@ const ProductService = {
     getAllProducts: async (request, h) => {
         try {
             return h.response(await products.find().execAsync());
-        }catch(err) {
+        } catch (err) {
             logger.error(err);
             return h.response().code(500);
         }
@@ -22,7 +29,7 @@ const ProductService = {
         try {
             const updated = await products.updateOne({ _id: params.product_id }, payload, { returnUpdatedDocs: true })
             return h.response(updated);
-        }catch(err) {
+        } catch (err) {
             logger.error(err);
             return h.response().code(500);
         }
